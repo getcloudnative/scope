5edc0469a2edc9df4200fef93fd100b731a7747f
Filip Barl <filip.barl@gmail.com>
2017-07-28 19:21:49 +0200

    Enabling multiple websocket connections.

--- client/app/scripts/reducers/root.js
+++ client/app/scripts/reducers/root.js
@@ -175,7 +175,7 @@ function clearNodes(state) {
   if (!topologyId) return state;
 
   return state
-    .updateIn(['nodesByTopology', topologyId], nodes => nodes.clear())
+    .setIn(['nodesByTopology', topologyId], makeMap())
     .set('nodesLoaded', false);
 }
 
